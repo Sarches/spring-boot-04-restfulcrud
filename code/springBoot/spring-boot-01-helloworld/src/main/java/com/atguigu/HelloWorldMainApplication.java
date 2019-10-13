be9aeeb035cb9b20a605d1f44a34ada497d9e433
sachs_glb <s123158@outlook.com>
2019-10-13 16:59:40 +0800

    commit demo

---
+++ code/springBoot/spring-boot-01-helloworld/src/main/java/com/atguigu/HelloWorldMainApplication.java
@@ -0,0 +1,10 @@
+package com.atguigu;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @SpringBootApplication 来标注一个主程序，说明这是一个Spring Boot 应用
+ */
+@SpringBootApplication
+public class HelloWorldMainApplication {
+}
