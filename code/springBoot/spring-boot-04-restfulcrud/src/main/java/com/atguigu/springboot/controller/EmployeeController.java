781a0e60e6f31a478c2700dd418e953c0d8d0e28
sachs_glb <s123158@outlook.com>
2019-10-27 00:44:27 +0800

    springbootcrud——CRUD员工添加功能完成

--- code/springBoot/spring-boot-04-restfulcrud/src/main/java/com/atguigu/springboot/controller/EmployeeController.java
+++ code/springBoot/spring-boot-04-restfulcrud/src/main/java/com/atguigu/springboot/controller/EmployeeController.java
@@ -1,11 +1,14 @@
 package com.atguigu.springboot.controller;
 
+import com.atguigu.springboot.dao.DepartmentDao;
 import com.atguigu.springboot.dao.EmployeeDao;
+import com.atguigu.springboot.entities.Department;
 import com.atguigu.springboot.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
 
@@ -14,12 +17,13 @@ public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
 
+    @Autowired
+    DepartmentDao departmentDao;
+
     //查询所有员工返回列表页面
     @GetMapping("/emps")
     public String list(Model model){
-
         Collection<Employee> employees = employeeDao.getAll();
-
         //放在请求域中
         model.addAttribute("emps",employees);
 
@@ -27,4 +31,25 @@ public class EmployeeController {
         //classpath:/templates/xxxx .html
         return "emp/list";
     }
+
+    @GetMapping("/emp")
+    public String toAddPage(Model model){
+        //来到添加页面,查出所有的部门，在页面显示
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts",departments);
+        return "emp/add";
+    }
+    //员工添加
+    //SpringMVC自动将请求参数和入参对象的属性进行一一绑定： 要求了请求参数的名字和javaBean 入参对象里面的属性名是一样的
+    @PostMapping("/emp")
+    public  String addEmp(Employee employee){
+        //来到员工列表页面
+        System.out.println("保存员工的信息:"+employee);
+        //保存员工
+        employeeDao.save(employee);
+        //redirect: 表示重定向到一个地址     /代表当前项目路径
+        //forward: 表示转发到一个地址
+        return "redirect:/emps";
+    }
+
 }
